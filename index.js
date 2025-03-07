bca21700d0beb6ed9e2808b8200b96f45617933f
Isaac Magalhães <isaaccunha>
2025-03-07 08:09:52 -0300

    deepseek 8b + express

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 import express from 'express'
 import ollama from 'ollama'
 import 'dotenv/config'
+import cors from 'cors'
 
 const app = express()
 const port = process.env.PORT || 3000
+
+app.use(cors())
 
 app.use(express.json())
 
 app.post('/chat', async (req, res) => {
   const { message } = req.body
 
+  console.log('Recebendo resposta do usuário')
+
   try {
     const response = await ollama.chat({
       model: 'deepseek-r1:8b',
       messages: [{ role: 'user', content: message }],
     })
+    
+    console.log('Resposta sendo processada pelo modelo')
 
     res.json({ reply: response.message.content })
+
+    console.log('resposta processada!')
   } catch (error) {
     res.status(500).json({error})
   }
